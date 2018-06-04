d385edd5c94f3ce6c46208e0d3a9c7602baa1bae
roujiawen <wenroujia@gmail.com>
2018-06-04 16:49:39 -0400

    Updated pmarginal so that its input does not depend on the values of the original matrix but only the number of dimensions. It outputs a filter matrix M rather than the filtered vector P_BA.

--- tools/math/pmarginal.m
+++ tools/math/pmarginal.m
@@ -1,29 +1,60 @@
-function M = pmarginal(A, order)
-    % similar to permute_sum, except:
-    % input can be flat or nonflat
-    % output will be a flattened column vector
-    % only for binary variables
-    
-    ndim = log2(length(A(:))); % number of dimensions of input
+function M = pmarginal(ndim, axes)
+% pmarginal Filter-sum matrix that takes the marginal of a 
+% joint probability vector (for binary variables) along
+% given axes. e.g. P_BA = M * P_ABC
+%
+% Inputs:
+% ndim  The dimension of the original probability vector 
+%       e.g. ndim(P_ABC) = 3
+% axes  A row vector specifying the axes to marginalize
+%       over. The order does matter.
+%
+% Outputs:
+% M     A sparse matrix which is the filter, of size 
+%       (length(filtered_vector), length(original_vector))
+%
+% Example:
+% >> pmarginal(3, [3 2])
+%
+% ans =
+% 
+%    (1,1)        1
+%    (1,2)        1
+%    (3,3)        1
+%    (3,4)        1
+%    (2,5)        1
+%    (2,6)        1
+%    (4,7)        1
+%    (4,8)        1
+%
+% -----------------------------------------------------
     
     if floor(ndim) ~= ndim
-       error('Error. Length of A must be a power of 2.')
+       error('Error. Dimension must be an integer.')
+    end
+    if any(floor(axes) ~= axes) || any(axes > ndim)
+       error('Error. AXES contains an invalid permutation index.')
     end
     
+    % Create index array A= [1, 2, 3, ..., 2^N]
+    ini_len = 2^ndim;
+    A = 1:ini_len;
+    
+    % Reshape A into either a N-D matrix or a column vector
     if ndim>1
-        A = reshape(A, ones(1, ndim)*2);
+        A = reshape(A, ones(1, ndim)*2); 
     else
-        A = A(:); % column vector
+        A = A(:);
     end
     
-    if any(floor(order) ~= order) || any(order > ndim)
-       error('Error. ORDER contains an invalid permutation index.')
-    end
-    
-    rest = setdiff(1:ndim, order); % e.g. order = [4 1 3]; rest = [2 5]
-    A = permute(A, [order rest]); % swap axes => shape:[7,2,5,3,11]
-    m = 2^length(order);
+    % Permute and reshape into 2D matrix with shape (m,n)
+    rest = setdiff(1:ndim, axes);
+    A = permute(A, [axes rest]); 
+    m = 2^length(axes);
     n = 2^length(rest);
-    A = reshape(A, [m, n]); % new shape: [7*2*5, 3*11]
-    M = A * ones(n, 1); % [7*2*5, 3*11] * [3*11, 1]
+    A = reshape(A, [m, n]);
+    
+    % Create filter matrix with shape (fin_len/m, ini_len)
+    M=sparse(repmat(1:m,1,n), A, ones(m,n), m, ini_len);
+    
 end
