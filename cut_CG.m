a5e7d63af0eba0ceed405c50a0a150e5668289c1
roujiawen <wenroujia@gmail.com>
2018-06-04 11:47:25 -0400

    Deleted files with wrong conditions and renamed cut_CG_kron_rewrite to cut_CG

--- cut_CG.m
+++ cut_CG.m
@@ -1,39 +1,26 @@
-% Triangle
-% Without using Kronecker Delta
-% Optimization variable in the shape of (2,2,2)
-% Handwritten rules
-% Conditions still in original P coordinates
+% Given:
+P_ABC = [0.5;0;0;0;0;0;0;0.5]; %(000, 100, 010, 110, 001, 101, 011, 111)
+ 
+% P_ABC in CG coordinate:
+G_ABC = p2cg(P_ABC);
 
-zero_vector = zeros(2,2,2);
+zero_vector = zeros(8,1);
+
 cvx_begin
-    variable x(2,2,2);
-    maximize(sum(sum(sum(zero_vector .* x))));
-    subject to
-        x(1,1,1) == 1;
-        x(2,1,1) == 0.5;
-        x(1,2,1) == 0.5;
-        x(1,1,2) == 0.5;
-        
-        x(2,2,1) == 0.25;
-        x(1,2,2) == 0.5;
-        x(2,1,2) == 0.5;
-        
-        %nonnegative conditions
-        x(2,2,2) >= 0;
-        x(2,2,1) - x(2,2,2) >= 0;
-        x(2,1,2) - x(2,2,2) >= 0;
-        x(1,2,2) - x(2,2,2) >= 0;
-        x(2,1,1) - x(2,2,1) - x(2,1,2) + x(2,2,2) >= 0;
-        x(1,2,1) - x(2,2,1) - x(1,2,2) + x(2,2,2) >= 0;
-        x(1,1,2) - x(1,2,2) - x(2,1,2) + x(2,2,2) >= 0;
-        x(1,1,1) - x(2,1,1) - x(1,2,1) - x(1,1,2) + x(2,2,1) + x(1,2,2) + x(2,1,2) - x(2,2,2) >= 0;        
+    variable x(8); % x is G_A2B1C1
+    minimize(zero_vector.' * x); 
+    subject to 
+        % Equality conditions
+        % G_A2C1 = G_AC
+        cgmarginal(x, [1 3]) == cgmarginal(G_ABC, [1 3]);
+        % G_B1C1 = G_BC
+        cgmarginal(x, [2 3]) == cgmarginal(G_ABC, [2 3]);
+        % G_A2B1 = S * P_A * P_B
+        cgmarginal(x, [1 2]) == p2cg((pmarginal(P_ABC, 1))*(pmarginal(P_ABC, 2)).');
+        % Nonnegative conditions
+        cg2p(x) >= 0;
 cvx_end
 
 % RESULTS
 % Status: Infeasible
 % Optimal value (cvx_optval): -Inf
-
-
-
-
-
