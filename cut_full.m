fa346f30df6f6d987d4c367ceea323eac9843ffd
roujiawen <wenroujia@gmail.com>
2018-06-05 09:41:59 -0400

    Replace handwritten matrices with those generated from pmarginal function

---
+++ cut_full.m
@@ -0,0 +1,47 @@
+% Everything is row-major (00, 01, 10, 11)
+P_ABC = [0.5;0;0;0;0;0;0;0.5];
+
+M_AC = pmarginal(3, [1 3]);
+M_BC = pmarginal(3, [2 3]);
+M_AB = pmarginal(3, [1 2]);
+    
+M_A = pmarginal(3, 1);
+M_B = pmarginal(3, 2);
+
+% cvx_begin
+%     variable x(8);
+%     subject to 
+%         M_AC * x == M_AC * P_ABC;
+%         M_BC * x == M_BC * P_ABC;
+%         M_AB * x == kron(M_A * P_ABC, M_B * P_ABC);
+%         x >= 0;
+% cvx_end
+
+%--------------LP standard form---------------
+A = [M_AC;
+     M_BC;
+     M_AB];
+ 
+b = [M_AC * P_ABC;
+     M_BC * P_ABC;
+     kron(M_A * P_ABC, M_B * P_ABC)];
+
+cvx_begin
+    variable x(8);
+    subject to 
+        A * x == b;
+        x >= 0;
+cvx_end
+
+% ------------Results---------------
+% Status: Infeasible
+% Optimal value (cvx_optval): +Inf
+
+
+%-----------Analysis----------------
+
+%rref(A)
+rank(full(A))%rank=7
+
+
+
