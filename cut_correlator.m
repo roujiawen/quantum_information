c55a29c8664095312ed6be5ff126d7290d1ca214
roujiawen <wenroujia@gmail.com>
2018-06-05 11:42:02 -0400

    Updated cut_correlator (calls to comarginal taking place of explicit matrices). Copied from cut_CG and only changed some function/variable names.

--- cut_correlator.m
+++ cut_correlator.m
@@ -1,57 +1,75 @@
+% Given:
 P_ABC = [0.5;0;0;0;0;0;0;0.5]; %(000, 100, 010, 110, 001, 101, 011, 111)
+ 
+% P_ABC in CG coordinate:
+C_ABC = p2co(3)*P_ABC;
 
-% Used for taking marginals of P_ABC
-M_AC = [1 0 1 0 0 0 0 0;
-        0 1 0 1 0 0 0 0;
-        0 0 0 0 1 0 1 0;
-        0 0 0 0 0 1 0 1]; %(00, 10, 01, 11)
-    
-M_BC = [1 1 0 0 0 0 0 0;
-        0 0 1 1 0 0 0 0;
-        0 0 0 0 1 1 0 0;
-        0 0 0 0 0 0 1 1]; %(00, 10, 01, 11)
-    
-M_A = [1 0 1 0 1 0 1 0;
-       0 1 0 1 0 1 0 1]; %(0, 1)
-   
-M_B = [1 1 0 0 1 1 0 0;
-       0 0 1 1 0 0 1 1]; %(0, 1)
+% Filter matrices
+M_AC = comarginal(3, [1 3]);
+M_BC = comarginal(3, [2 3]);
+M_AB = comarginal(3, [1 2]);
 
-% Change of basis matrix
-S = [1 1;
-     1 -1];
-% Inverse change of basis matrix
-S_inv = inv(S);
+% cvx_begin
+%     variable x(8); % x is G_A2B1C1
+%     subject to 
+%         % Equality conditions
+%         % G_A2C1 = G_AC
+%         M_AC * x == M_AC * C_ABC;
+%         % G_B1C1 = G_BC
+%         M_BC * x == M_BC * C_ABC;
+%         % G_A2B1 = S * P_A * P_B
+%         M_AB * x == kron(comarginal(3, [1]) * C_ABC, comarginal(3, [2]) * C_ABC);
+%         % Nonnegative conditions
+%         co2p(3)*x >= 0;
+% cvx_end
 
-% Filtering for P_iii 
-F_AC = [1 0 0 0 0 0 0 0;
-        0 1 0 0 0 0 0 0;
-        0 0 0 0 1 0 0 0;
-        0 0 0 0 0 1 0 0];
-F_BC = [1 0 0 0 0 0 0 0;
-        0 0 1 0 0 0 0 0;
-        0 0 0 0 1 0 0 0;
-        0 0 0 0 0 0 1 0];
-F_AB = [1 0 0 0 0 0 0 0;
-        0 1 0 0 0 0 0 0;
-        0 0 1 0 0 0 0 0;
-        0 0 0 1 0 0 0 0];
 
-zero_vector = zeros(8,1);
+%--------------General form---------------
+
+% Concatenate conditions
+A = [M_AC;
+     M_BC;
+     M_AB];
+b = [M_AC * C_ABC;
+     M_BC * C_ABC;
+     kron(comarginal(3, [1]) * C_ABC, comarginal(3, [2]) * C_ABC)];
+G = -co2p(3);
+h = 0;
+ 
+% cvx_begin
+%     variable x(8);
+%     subject to 
+%         % Equality conditions
+%         A * x == b;
+%         % Inequality conditions
+%         G * x <= h;
+% cvx_end
+
+
+%--------------LP standard form---------------
+A_ = [G eye(8);
+      A zeros(12,8)];
+b_ = [zeros(8,1);
+      b];
 
 cvx_begin
-    variable x(8);
-    maximize(zero_vector.' * x); % x is P_iii
-    subject to 
+    variable x(16);
+    subject to
         % Equality conditions
-        kpow(S_inv, 2) * (F_AC * x) == M_AC * P_ABC; % (00, 10, 01, 11)
-        kpow(S_inv, 2) * (F_BC * x) == M_BC * P_ABC; % (00, 10, 01, 11)
-        kpow(S_inv, 2) * (F_AB * x) == reshape((M_A * P_ABC)*(M_B * P_ABC).', [4,1]);
-                                                    %(00, 10, 01, 11)
+        A_ * x - b_ == 0;
         % Nonnegative conditions
-        kpow(S_inv, 3) * x >= 0;
+        x >= 0;
 cvx_end
+
+
 
 % RESULTS
 % Status: Infeasible
-% Optimal value (cvx_optval): -Inf
+% Optimal value (cvx_optval): +Inf
+
+%--------------Analysis---------------
+fprintf('Rank of A = %d\n', rank(full(A)));
+
+fprintf('Rank of A'' = %d\n', rank(full(A_)));
+
+
