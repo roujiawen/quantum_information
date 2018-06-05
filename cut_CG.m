7da8bbafe8fb1c82378d194b6a1f94ab99a7c4a6
roujiawen <wenroujia@gmail.com>
2018-06-05 10:00:27 -0400

    Modified independence condition right-hand side to be a kron of two CG vectors

--- cut_CG.m
+++ cut_CG.m
@@ -3,8 +3,6 @@ P_ABC = [0.5;0;0;0;0;0;0;0.5]; %(000, 100, 010, 110, 001, 101, 011, 111)
  
 % P_ABC in CG coordinate:
 G_ABC = p2cg(3)*P_ABC;
-P_A = pmarginal(3, 1) * P_ABC;
-P_B = pmarginal(3, 2) * P_ABC;
 
 % Filter matrices
 M_AC = cgmarginal(3, [1 3]);
@@ -20,7 +18,7 @@ M_AB = cgmarginal(3, [1 2]);
 %         % G_B1C1 = G_BC
 %         M_BC * x == M_BC * G_ABC;
 %         % G_A2B1 = S * P_A * P_B
-%         M_AB * x == p2cg(2)*reshape(P_A* P_B.', [4,1]);
+%         M_AB * x == kron(cgmarginal(3, [1]) * G_ABC, cgmarginal(3, [2]) * G_ABC);
 %         % Nonnegative conditions
 %         cg2p(3)*x >= 0;
 % cvx_end
@@ -34,18 +32,18 @@ A = [M_AC;
      M_AB];
 b = [M_AC * G_ABC;
      M_BC * G_ABC;
-     p2cg(2)*reshape(P_A* P_B.', [4,1])];
+     kron(cgmarginal(3, [1]) * G_ABC, cgmarginal(3, [2]) * G_ABC)];
 G = -cg2p(3);
 h = 0;
  
-cvx_begin
-    variable x(8);
-    subject to 
-        % Equality conditions
-        A * x == b;
-        % Nonnegative conditions
-        G * x <= h;
-cvx_end
+% cvx_begin
+%     variable x(8);
+%     subject to 
+%         % Equality conditions
+%         A * x == b;
+%         % Nonnegative conditions
+%         G * x <= h;
+% cvx_end
 
 
 %--------------LP standard form---------------
