cd2b5122a6da28242e1b580b592c5a6da2ddbb8c
roujiawen <wenroujia@gmail.com>
2018-06-20 16:45:45 -0400

    Added optional argument for passing in pre-allocated space for storing results

--- tools/math/dot2.m
+++ tools/math/dot2.m
@@ -1,26 +1,36 @@
-function C = dot2(A, B, d)
+function C = dot2(A, B, C)
 % Take the pair-wise dot product of two lists of matrices
 % Returns a list of matrices which are the products. 
-% The list has length size(A)(d)
-% d specifies the outer axis (that links matrices together)
-% Only d=3 is implemented so far.
+% The list has length size(A)(3)
+% Axis 3 is the list axis (outermost)
+%
+% Inputs
+%       A  : 3D matrix of shape (m,k,l)
+%       B  : 3D matrix of shape (k,n,l)
+%       C  : optional, could be used for passing in
+%            pre-allocated space for storing results
+% -------------------------------------------
 
-if (nargin > 2) && (d ~= 3)
-    error('Error. Not implemented.')
-end
 sA = size(A);
 sB = size(B);
-% Length along outer axis
+
+% Length along outermost axis
 lA = sA(3);
 lB = sB(3);
 if lA ~= lB
     error('Error. A and B must have the same length along the d-th axis.')
 end
+
 % Dimension of final matrix
 mA = sA(1);
 nB = sB(2);
-% Allocate empty space for C
-C = zeros(mA, nB, lA);
+
+% If C is not provided, allocate empty space for results
+if nargin < 3
+    C = zeros(mA, nB, lA);
+end
+
+% Calculate dot products
 for i = 1 : lA
     C(:,:,i) = A(:,:,i) * B(:,:,i);
 end
--- tools/math/trace2.m
+++ tools/math/trace2.m
@@ -1,17 +1,25 @@
-function tr = trace2(A, d)
+function tr = trace2(A, tr)
 % Take the trace of each matrix in a list
-% Returns the traces, a column vector of length size(A)(d)
-% d specifies the outer axis (that links matrices together)
-% Only d=3 is implemented so far.
+% Returns the traces, a column vector of length size(A)(3)
+% Axis 3 is the list axis (outermost)
+%
+% Inputs
+%       A  : 3D matrix of shape (m,m,n)
+%       tr : optional, could be used for passing in
+%            pre-allocated space for storing results
+% -------------------------------------------
 
-if (nargin > 1) && (d ~= 3)
-    error('Error. Not implemented.')
-end
 s = size(A);
-% Length along outer axis
+
+% Length along outermost axis
 n = s(3);
-% Allocate empty space for traces
-tr = zeros(n,1);
+
+% If tr is not provided, allocate empty space for results
+if nargin < 2
+    tr = zeros(n,1);
+end
+
+% Calculate traces
 for i = 1 : n
     tr(i) = trace(A(:,:,i));
 end
