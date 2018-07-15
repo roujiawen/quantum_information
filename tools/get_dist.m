cd39a2d0569b517185566968c0e149b061ca9f1d
roujiawen <wenroujia@gmail.com>
2018-07-15 15:03:55 -0400

    Added more feasible distributions

--- tools/get_dist.m
+++ tools/get_dist.m
@@ -2,15 +2,35 @@ function P = get_dist(name)
 % Returns one of the following distributions:
 % - 'W-type'
 % - 'complete correlation'
+% - 'uniform'
+% - 'anti-correlation'
 % - 'EJM'
 
     switch name
+        case "uniform"
+            P = 1/8 * ones(8,1);
+        case "anti-correlation"
+            P = 1/6 * [0;1;1;1;1;1;1;0];
         case "W-type"
             P = [0;1/3;1/3;0;1/3;0;0;0];
         case "complete correlation"
             P = [0.5;0;0;0;0;0;0;0.5];
         case "EJM"
             P = gen_ejm();
+        case "half-A"
+            % 3 9 33
+            P = gen_half(4, 2);
+        case "half-B"
+            P = gen_half(4, 5);
+        case "half-C"
+            P = gen_half(4, 17);
+        case "out2half-A"
+            % 3 9 33
+            P = gen_half(2, 2);
+        case "out2half-B"
+            P = gen_half(2, 3);
+        case "out2half-C"
+            P = gen_half(2, 5);
     end
 end
 
@@ -26,4 +46,10 @@ function P = gen_ejm()
         M(i,i,i) = 25/256;
     end
     P = M(:);
+end
+
+function P = gen_half(nout, k)
+    P = zeros(nout^3,1);
+    P(1) = 0.5;
+    P(k) = 0.5;
 end
