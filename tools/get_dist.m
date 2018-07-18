b7aa5b602d8508bce22ccbb147b726aa201998ea
roujiawen <wenroujia@gmail.com>
2018-07-18 13:18:24 -0400

    Made compatible with 2015a; added failed feasible distributions; added nout and feasibility as additional outputs

--- tools/get_dist.m
+++ tools/get_dist.m
@@ -1,36 +1,56 @@
-function P = get_dist(name)
+function [P, nout, feasibility] = get_dist(name)
 % Returns one of the following distributions:
-% - 'W-type'
-% - 'complete correlation'
+% FEASIBLE:
 % - 'uniform'
 % - 'anti-correlation'
 % - 'EJM'
+% - 'fail1'
+% - 'fail2'
+% - 'fail3'
+% INFEASIBLE:
+% - 'W-type'
+% - 'complete correlation'
+% - 'anti-correlation'
 
     switch name
-        case "uniform"
+        case 'uniform'
             P = 1/8 * ones(8,1);
-        case "anti-correlation"
+            nout = 2;
+            feasibility = true;
+        case 'anti-correlation'
             P = 1/6 * [0;1;1;1;1;1;1;0];
-        case "W-type"
+            nout = 2;
+            feasibility = true;
+        case 'fail1'
+            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            trial = 10;
+            P = test_data(:,trial);
+            nout = 4;
+            feasibility = true;
+        case 'fail2'
+            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            trial = 73;
+            P = test_data(:,trial);
+            nout = 4;
+            feasibility = true;
+        case 'fail3'
+            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            trial = 88;
+            P = test_data(:,trial);
+            nout = 4;
+            feasibility = true;
+        case 'W-type'
             P = [0;1/3;1/3;0;1/3;0;0;0];
-        case "complete correlation"
+            nout = 2;
+            feasibility = false;
+        case 'complete correlation'
             P = [0.5;0;0;0;0;0;0;0.5];
-        case "EJM"
+            nout = 2;
+            feasibility = false;
+        case 'EJM'
             P = gen_ejm();
-        case "half-A"
-            % 3 9 33
-            P = gen_half(4, 2);
-        case "half-B"
-            P = gen_half(4, 5);
-        case "half-C"
-            P = gen_half(4, 17);
-        case "out2half-A"
-            % 3 9 33
-            P = gen_half(2, 2);
-        case "out2half-B"
-            P = gen_half(2, 3);
-        case "out2half-C"
-            P = gen_half(2, 5);
+            nout = 4;
+            feasibility = true;
     end
 end
 
@@ -48,8 +68,3 @@ function P = gen_ejm()
     P = M(:);
 end
 
-function P = gen_half(nout, k)
-    P = zeros(nout^3,1);
-    P(1) = 0.5;
-    P(k) = 0.5;
-end
