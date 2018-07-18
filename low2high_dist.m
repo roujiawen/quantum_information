3c7c7d67ded5b33efaf4232f699d00a27dead271
roujiawen <wenroujia@gmail.com>
2018-07-18 13:16:40 -0400

    Implemented infeasible; reduce redundant input args

--- low2high_dist.m
+++ low2high_dist.m
@@ -1,18 +1,16 @@
-function h_dist = low2high_dist(l_dist, l_out, h_out, infea, conv_mat)
+function h_dist = low2high_dist(dist_name, h_out, conv_mat)
 % LOW2HIGH_DIST generate random distribution with higher number of 
 % outcomes which has the same feasibility with a corresponding 
 % distribution with lower number of outcomes.
 % Assumes 3 observed variables (triangle scenario)
 nvar = 3;
 
-% default input: infea = false
-if nargin < 4
-    infea = false;
-end
+% Get original (lower-outcome) distribution
+[l_dist, l_out, feasibility] = get_dist(dist_name);
 
 % If conversion matrices (e.g. P(a'|a)) not given, generate randomly
 if nargin < 5
-    if infea == false
+    if feasibility == true
         % For feasible distribution, generate random matrix
         conv_mat = rand(h_out, l_out, nvar);
 
@@ -23,8 +21,19 @@ if nargin < 5
 
     else
         % For infeasible distribution, generate random invertible matrix
+        conv_mat = rand(h_out, l_out, nvar);
+        
+        % Make sure all matrices are invertible (is this necessary??)
+        for k = 1:nvar
+            while rank(conv_mat(:,:,k)) < l_out
+                conv_mat(:,:,k) = rand(h_out,l_out)
+            end
+        end
+
         % Normalize probabilities
-        error("Not implemented");
+        col_sums = sum(conv_mat, 1);
+        divisor = repmat(col_sums, h_out, 1, 1);
+        conv_mat = conv_mat ./ divisor;
     end
 end
 
@@ -46,4 +55,4 @@ h_dist = kron_prod * l_dist;
 % h_dist(find(h_dist))
 
 end
-% call h_out = 4; h_dist = low2high_dist(get_dist("uniform"), 2, h_out);cvx_spiral(h_out, h_dist, 'full', 1)
+% call h_out = 4; h_dist = low2high_dist(get_dist('uniform'), 2, h_out);cvx_spiral(h_out, h_dist, 'full', 1)
