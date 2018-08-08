5fcaf07c81ca018057e28bdde1d04dfad6a4c218
roujiawen <wenroujia@gmail.com>
2018-08-08 18:22:35 -0400

    automatically strip '.mat', added example

--- tools/mat2csv.m
+++ tools/mat2csv.m
@@ -1,6 +1,10 @@
 function mat2csv(filepath)
 % Assuming the variable name is 'data'
-
+% Example:
+% mat2csv('results/5-500trials_v2/rand_fea_500trials')
 load(filepath, 'data');
+if strcmp(extractAfter(filepath,length(filepath)-4), '.mat')
+    filepath = extractBefore(filepath,length(filepath)-3);
+end
 cell2csv([filepath '.csv'], data);
 end
