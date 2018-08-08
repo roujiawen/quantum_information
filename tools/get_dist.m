5cd79c24357a6c9a41c48da059392c1b9b097c01
roujiawen <wenroujia@gmail.com>
2018-08-08 18:21:45 -0400

    added trial_id to access 500trials

--- tools/get_dist.m
+++ tools/get_dist.m
@@ -1,4 +1,4 @@
-function [P, nout, feasibility] = get_dist(name, use_interval)
+function [P, nout, feasibility] = get_dist(name, use_interval, trial_id)
 % Returns one of the following distributions:
 % FEASIBLE:
 % - 'uniform'
@@ -11,20 +11,37 @@ function [P, nout, feasibility] = get_dist(name, use_interval)
 % - 'W-type'
 % - 'complete correlation'
 
-if nargin < 2
-    use_interval = false;%default
+if (nargin < 2) || isempty(use_interval)
+    use_interval = false; %default
+end
+
+if strcmp(name, '500trials')
+    %from feasible distributions only
+    load('logs/4-500trials/test_data_rand_fea_500trials.mat', 'test_data');
+    P = test_data(:, trial_id);
+    nout = 4;
+    feasibility = true;
+    return;
 end
 
 if use_interval
     switch name
         case 'uniform'
-            P = 1/8 * intval(ones(8,1));
+            P = intval(ones(8,1))/8;
             nout = 2;
             feasibility = true;
         case 'complete correlation'
-            P = 1/2 * intval([1;0;0;0;0;0;0;1]);
+            P = intval([1;0;0;0;0;0;0;1])/2;
             nout = 2;
             feasibility = false;
+        case 'W-type'
+            P = intval([0;1;1;0;1;0;0;0])/3;
+            nout = 2;
+            feasibility = false;
+        case 'EJM'
+            P = gen_ejm_use_interval();
+            nout = 4;
+            feasibility = true;
         otherwise
             error('Not implemented');
     end
@@ -39,19 +56,19 @@ else
             nout = 2;
             feasibility = true;
         case 'fail1'
-            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            load('logs/test_data_100trials/test_data_4.mat', 'test_data');
             trial = 10;
             P = test_data(:,trial);
             nout = 4;
             feasibility = true;
         case 'fail2'
-            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            load('logs/test_data_100trials/test_data_4.mat', 'test_data');
             trial = 73;
             P = test_data(:,trial);
             nout = 4;
             feasibility = true;
         case 'fail3'
-            load('outs/test_data_100trials/test_data_4.mat', 'test_data');
+            load('logs/test_data_100trials/test_data_4.mat', 'test_data');
             trial = 88;
             P = test_data(:,trial);
             nout = 4;
@@ -88,3 +105,15 @@ function P = gen_ejm()
     P = M(:);
 end
 
+function P = gen_ejm_use_interval()
+    M = intval(ones(4,4,4))*5/256;
+    for i = 1:4
+        for j = 1:4
+            M(i,i,j) = intval(1)/256;
+            M(i,j,i) = intval(1)/256;
+            M(j,i,i) = intval(1)/256;
+        end
+        M(i,i,i) = intval(25)/256;
+    end
+    P = M(:);
+end
