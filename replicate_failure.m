37dfad725e467c051457b9a55e2f54cb9a7ef73e
roujiawen <wenroujia@gmail.com>
2018-08-08 18:21:23 -0400

    investigated failure

--- replicate_failure.m
+++ replicate_failure.m
@@ -1,8 +1,31 @@
 nout = 4;
-solver = 'MSK_OPTIMIZER_INTPNT';
+solver = 'Mosek_INTPNT';
 slack = 3;
 
+OPTIONS = struct();
+%OPTIONS.MSK_IPAR_INTPNT_BASIS = 0;
 
-cvx_spiral(4, get_dist('fail1'), solver, 'full', 3);
-%cvx_spiral(4, get_dist('fail2'), solver, 'CG', 3);
-%cvx_spiral(4, get_dist('fail3'), solver, 'full', 3);
+% CVX FAILS:
+%cvx_spiral(nout, get_dist('fail1'), solver, 'CG', slack);
+%cvx_spiral(nout, get_dist('fail2'), solver, 'CG', slack);
+%cvx_spiral(nout, get_dist('fail3'), solver, 'full', slack);
+%cvx_spiral(nout, get_dist('500trials', [], 44), solver, 'CG', slack);
+%cvx_spiral(nout, get_dist('500trials', [], 55), solver, 'CG', slack);%!!!
+%cvx_spiral(nout, get_dist('500trials', [], 174), solver, 'CG', slack);%!!!
+%cvx_spiral(nout, get_dist('500trials', [], 228), solver, 'cor', slack);
+
+% VSDP ALSO FAILS:
+OPTIONS.MOSEK_OPTIONS.MSK_IPAR_BI_MAX_ITERATIONS = 50000;
+[objt,xt,yt,zt,info] = mysdps_spiral(nout, get_dist('fail1'), 'mosek', 'CG', slack, OPTIONS);
+
+%OPTIONS.MOSEK_OPTIONS.MSK_IPAR_INTPNT_BASIS = 0;
+
+%[objt1,xt1,yt1,zt1,info1] = mysdps_spiral(nout, get_dist('fail1'), 'mosek', 'CG', slack, OPTIONS);
+% mysdps_spiral(nout, get_dist('fail3'), 'mosek', 'full', slack, OPTIONS);
+
+% CVX FAILS BUT MOSEK DOESN'T:
+%mysdps_spiral(nout, get_dist('fail2'), 'mosek', 'CG', slack, OPTIONS);
+
+
+%%%%%%%%%%%%%MOSEK%%%%%%%%%%%%%%%%%
+%sol = mosek_spiral(nout, get_dist('fail1'), 'full', slack)
