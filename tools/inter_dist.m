ace691508bd74029ffbfc6b55a71aa20c1df7a56
roujiawen <wenroujia@gmail.com>
2018-08-08 18:22:02 -0400

    added new args dist_A and dist_B

--- tools/inter_dist.m
+++ tools/inter_dist.m
@@ -1,15 +1,22 @@
-function dist = inter_dist(eta, use_interval)
+function dist = inter_dist(eta, dist_A, dist_B, use_interval)
 % INTER_DIST Interpolate a mid point between two distributions, given
 % parameter eta
 
-if nargin < 2
+if (nargin < 3) || isempty(dist_A) || isempty(dist_B)
+    % infeasible
+    dist_A = get_dist('complete correlation', use_interval);
+    % feasible
+    dist_B = get_dist('uniform', use_interval);
+end
+
+if nargin < 4
     use_interval = false;%default
 end
 
-% infeasible
-dist_A = get_dist('complete correlation', use_interval);
-% feasible
-dist_B = get_dist('uniform', use_interval);
+if use_interval
+    eta = intval(eta);
+end
+
 % Take the convex combination
 dist = (1-eta) * dist_A + eta * dist_B;
 
