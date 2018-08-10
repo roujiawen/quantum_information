5fa62d44c398d11260721059c3367bf3349112ea
roujiawen <wenroujia@gmail.com>
2018-08-10 17:22:03 -0400

    temp test code

--- trials_vsdp/use_mosek.m
+++ trials_vsdp/use_mosek.m
@@ -18,48 +18,15 @@
 % K.l = n;
 % A = A';
 
-%-------------- cut inflation ---------------------
-[A, b, G, h] = get_cut_constraints('anti-correlation', 'CG');
-[m, n] = size(A);
-K.l = n;
-c = zeros(n,1);
-A = A';
+%-------------- spiral inflation ---------------------
+const_type = 'ineq';
+for name = {'W-type', 'complete correlation', 'anti-correlation', 'uniform', 'EJM', 'fail1'}
+    fprintf('---------------%s---------------\n', name{1});
+    [A, b] = get_spiral_form(const_type, 'corr', get_dist(name{1}));
+    mosek_wrapper(A,b,const_type);
+end
 
 
-clear mosek_prob;
-mosek_prob.c = full(c);
-mosek_prob.a = sparse(A)';
-mosek_prob.blc = full(b);
-mosek_prob.buc = full(b);
-mosek_prob.blx = zeros(length(c),1);
-mosek_prob.bux = inf(length(c),1);
-param = struct();
-%param.MSK_IPAR_LOG = 0;
-tolerance = 0;
 
-param.MSK_DPAR_INTPNT_CO_TOL_PFEAS = tolerance;
-param.MSK_DPAR_INTPNT_CO_TOL_DFEAS = tolerance;
-param.MSK_DPAR_INTPNT_CO_TOL_REL_GAP = tolerance;
-param.MSK_DPAR_INTPNT_CO_TOL_INFEAS = tolerance;
-param.MSK_DPAR_INTPNT_CO_TOL_MU_RED = tolerance;
-param.MSK_DPAR_INTPNT_TOL_PFEAS = tolerance;
-param.MSK_DPAR_INTPNT_TOL_DFEAS = tolerance;
-param.MSK_DPAR_INTPNT_TOL_INFEAS = tolerance;
-%param.MSK_DPAR_INTPNT_TOL_REL_GAP = tolerance; %max( 1e-14, tolerance)
 
-[r, res] = mosekopt('minimize', mosek_prob, param);
-sol   = res.sol;
-sol.itr.solsta
 
-% Interior-point solution.
-
-% sol.itr.xx'      % x solution.
-% sol.itr.sux'     % Dual variables corresponding to buc.
-% sol.itr.slx'     % Dual variables corresponding to blx.
-
-% Basic solution.
-
-%sol.bas.xx'      % x solution in basic solution.
-
-%[x,tmp,flag,tmp,lambda] = linprog(full(c),[],[],A',full(b),zeros(length(c),1),inf(length(c),1));
-    
