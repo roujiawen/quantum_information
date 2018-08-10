8aaf4971d15238e3f80b15c39f1508c0639578b1
roujiawen <wenroujia@gmail.com>
2018-08-10 14:41:31 -0400

    completed equality constraints; removed redundant rows; tested;

--- get_spiral_form.m
+++ get_spiral_form.m
@@ -1,49 +1,122 @@
-function get_spiral_form(ctype, basis, P_ABC)
+function [constraint_A, constraint_b] = get_spiral_form(ctype, basis, P_ABC)
 % GET_SPIRAL_FORM returns formulation for the spiral inflation, given
 % the constraint type {'eq', 'ineq'} and basis {'full', 'CG', 'corr'}
+
+% For debugging
+if nargin == 0
+    ctype = 'eq'; basis = 'CG'; P_ABC = ones(8,1)/8;
+end
+    
+P_ABC = P_ABC(:);
 nvar = 3;
-nout = length(P_ABC)^(1/nvar);
+nout = round(length(P_ABC)^(1/nvar));
 
-function mar_matrix = M_inf(varargin)
-    keys =   {'A1','B1','C1','A2','B2','C2'};
-    dict = containers.Map(keys, 1:6);
-    indices = zeros(1, nargin);
-    for k = 1:nargin
-        indices(k) = dict(varargin{k});
-    end
-    mar_matrix = sum_marginal(nout, 6, indices);
-end
+% Aliases for readibility
+A1=1; B1=2; C1=3; A2=4; B2=5; C2=6;
+A=1; B=2; C=3;
 
-function mar_P = P_ori(varargin)
-    keys =   {'A', 'B', 'C'};
-    dict = containers.Map(keys, 1:3);
-    indices = zeros(1, nargin);
-    for k = 1:nargin
-        indices(k) = dict(varargin{k});
+    function mar_matrix = M_inf(varargin)
+        %A1=1, B1=2, C1=3, A2=4, B2=5, C2=6
+        indices = cell2mat(varargin);
+        mar_matrix = sum_marginal(nout, 6, indices);
     end
-    mar_P = sum_marginal(nout, 3, indices) * P_ABC;
-end
+
+    function mar_P = P_ori(varargin)
+        %A=1, B=2, C=3
+        indices = cell2mat(varargin);
+        mar_P = sum_marginal(nout, nvar, indices) * P_ABC;
+    end
+
+    function mar_matrix = K_inf(varargin)
+        %A1=1, B1=2, C1=3, A2=4, B2=5, C2=6
+        indices = cell2mat(varargin);
+        mar_matrix = slice_marginal(nout, 6, indices);
+    end
+
+    function mar_P = G_ori(varargin)
+        %A=1, B=2, C=3
+        indices = cell2mat(varargin);
+        mar_P = slice_marginal(nout, nvar, indices) * G_ABC;
+    end
+
+    function mar_P = C_ori(varargin)
+        %A=1, B=2, C=3
+        indices = cell2mat(varargin);
+        mar_P = slice_marginal(nout, nvar, indices) * C_ABC;
+    end
+
+    function fil = red_rows_filter
+        % Filtering redundant rows in CG and correlator bases
+        cube = ones(nout, nout, nout); % cube of size nout^3
+        cube(1,:,:) = 0;
+        cube(:,1,1) = 0;
+        sub1 = ones(nout^3,1);
+        sub2 = cube(:);
+        fil = logical([sub1; sub2; sub2; sub2; sub1]);
+    end
 
 switch ctype
     case 'eq'
         switch basis
             case 'full'
-                % x = P(A1,B1,C1,A2,B2,C2)
-                A = [M_inf('A1','B1','C1');
-                     M_inf('A1','B2','C2');
-                     M_inf('B1','C2','A2');
-                     M_inf('C1','A2','B2');
-                     M_inf('A2','B2','C2')];
-                b = [P_ABC;
-                     kron(P_ori('C'), P_ori('A','B'));
-                     kron(P_ori('A'), P_ori('B','C'));
-                     kron(P_ori('B'), P_ori('C','A'));
-                     kron(P_ori('C'), kron(P_ori('B'), P_ori('A')))
-                    ];
-            case 'CG'
+                % M_inf * x = P_ABC  =>  A * x = b
+                % where x = P(A1,B1,C1,A2,B2,C2)
+                % M_inf : matrix that sums over the marginal of inflation
                 constraint_A = ...
-                    [
+                    [M_inf(A1,B1,C1);
+                    M_inf(A1,B2,C2);
+                    M_inf(B1,C2,A2);
+                    M_inf(C1,A2,B2);
+                    M_inf(A2,B2,C2)];
+                constraint_b = ...
+                    [P_ori(A,B,C);
+                    kron(P_ori(C), P_ori(A,B));
+                    kron(P_ori(A), P_ori(B,C));
+                    kron(P_ori(B), P_ori(C,A));
+                    kron(P_ori(C), kron(P_ori(B), P_ori(A)))];
+            case 'CG'
+                % K_inf * S_CG * x = G_ABC  =>  A * x = b
+                % where x = P(A1,B1,C1,A2,B2,C2)
+                % K_inf : matrix that picks the marginal of inflation
+                G_ABC = switch_basis_mat('full', 'CG', nout, nvar) * P_ABC;
+                S_CG = switch_basis_mat('full', 'CG', nout, 6);
+                
+                constraint_A = ...
+                    [K_inf(A1,B1,C1);
+                    K_inf(A1,B2,C2);
+                    K_inf(B1,C2,A2);
+                    K_inf(C1,A2,B2);
+                    K_inf(A2,B2,C2)] * S_CG;
+                constraint_b = ...
+                    [G_ori(A,B,C);
+                    kron(G_ori(C), G_ori(A,B));
+                    kron(G_ori(A), G_ori(B,C));
+                    kron(G_ori(B), G_ori(C,A));
+                    kron(G_ori(C), kron(G_ori(B), G_ori(A)))];
+                constraint_A = constraint_A(red_rows_filter, :);
+                constraint_b = constraint_b(red_rows_filter, :);
             case 'corr'
+                % K_inf * S_corr * x = C_ABC  =>  A * x = b
+                % where x = P(A1,B1,C1,A2,B2,C2)
+                % K_inf : matrix that picks the marginal of inflation
+                C_ABC = switch_basis_mat('full', 'corr', nout, nvar) * P_ABC;
+                S_corr = switch_basis_mat('full', 'corr', nout, 6);
+                
+                constraint_A = ...
+                    [K_inf(A1,B1,C1);
+                    K_inf(A1,B2,C2);
+                    K_inf(B1,C2,A2);
+                    K_inf(C1,A2,B2);
+                    K_inf(A2,B2,C2)] * S_corr;
+                constraint_b = ...
+                    [C_ori(A,B,C);
+                    kron(C_ori(C), C_ori(A,B));
+                    kron(C_ori(A), C_ori(B,C));
+                    kron(C_ori(B), C_ori(C,A));
+                    kron(C_ori(C), kron(C_ori(B), C_ori(A)))];
+                
+                constraint_A = constraint_A(red_rows_filter, :);
+                constraint_b = constraint_b(red_rows_filter, :);
             otherwise
                 error('Not implemented');
         end
@@ -52,6 +125,23 @@ switch ctype
             case 'full'
                 error('Not implemented');
             case 'CG'
+                % K_inf * S_CG * x = G_ABC  =>  A * x = b
+                % where x = P(A1,B1,C1,A2,B2,C2)
+                % K_inf : matrix that picks the marginal of inflation
+                G_ABC = switch_basis_mat('full', 'CG', nout, nvar) * P_ABC;
+                
+                constraint_A = ...
+                    [K_inf(A1,B1,C1);
+                    K_inf(A1,B2,C2);
+                    K_inf(B1,C2,A2);
+                    K_inf(C1,A2,B2);
+                    K_inf(A2,B2,C2)];
+                constraint_b = ...
+                    [G_ori(A,B,C);
+                    kron(G_ori(C), G_ori(A,B));
+                    kron(G_ori(A), G_ori(B,C));
+                    kron(G_ori(B), G_ori(C,A));
+                    kron(G_ori(C), kron(G_ori(B), G_ori(A)))];
             case 'corr'
             otherwise
                 error('Not implemented');
