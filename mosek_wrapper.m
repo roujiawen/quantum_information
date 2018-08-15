7e590c8533106f1d74370059bade0b079d8bb291
roujiawen <wenroujia@gmail.com>
2018-08-14 20:45:38 -0400

    Turn the standard eq and ineq LP problem --> mosek format and call mosek

--- mosek_wrapper.m
+++ mosek_wrapper.m
@@ -1,19 +1,15 @@
-function mosek_wrapper(A, b, const_type)
+function [res, time] = mosek_wrapper(A, b, const_type)
 
 if nargin < 3
     const_type = 'eq';
 end
 clear mosek_prob;
-clear K;
 switch const_type
     case 'eq'
         % A * x = b
         % x > 0
         [m, n] = size(A);
-        K.l = n;
-        c = zeros(n,1);
-        
-        mosek_prob.c = full(c);
+        mosek_prob.c = zeros(n,1);
         mosek_prob.a = sparse(A);
         mosek_prob.blc = full(b);
         mosek_prob.buc = full(b);
@@ -22,10 +18,7 @@ switch const_type
     case 'ineq'
         % A * x > b
         [m, n] = size(A);
-        K.f = n;
-        c = zeros(n,1);
-        
-        mosek_prob.c = full(c);
+        mosek_prob.c = zeros(n,1);
         mosek_prob.a = sparse(A);
         mosek_prob.blc = full(b);
         mosek_prob.buc = inf(m, 1);
@@ -33,11 +26,10 @@ switch const_type
         mosek_prob.bux = inf(n,1);
 end
 
-
-
-
-
 param = struct();
+%-----------SOLVER-------------
+%param.MSK_IPAR_INTPNT_BASIS = 0;
+%param.MSK_IPAR_OPTIMIZER = 'MSK_OPTIMIZER_PRIMAL_SIMPLEX';
 
 %-----------VERBOSE------------
 param.MSK_IPAR_LOG = 0;
@@ -53,10 +45,22 @@ param.MSK_IPAR_LOG = 0;
 % param.MSK_DPAR_INTPNT_TOL_DFEAS = tolerance;
 % param.MSK_DPAR_INTPNT_TOL_INFEAS = tolerance;
 % param.MSK_DPAR_INTPNT_TOL_REL_GAP = tolerance; %max( 1e-14, tolerance)
+tic;
+[rcode, res] = mosekopt('minimize info', mosek_prob, param);
+time = toc;
 
-[r, res] = mosekopt('minimize', mosek_prob, param);
-sol   = res.sol;
-disp([sol.itr.solsta '   ' sol.itr.prosta]);
+% x = res.sol.itr.xx;
+% if ~isempty(x)
+%     y = res.sol.itr.y;
+%     z = c - A*y;
+%     obj(1) = c'*x;
+%     obj(2) = b'*y;
+% end
+% if (res.sol.itr.solsta == 1) && (res.sol.itr.prosta == 1)
+%     info = 0;
+% else
+%     info = -1;
+% end
 
 % Interior-point solution.
 
