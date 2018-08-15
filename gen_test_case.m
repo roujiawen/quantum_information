8fdd426ca043c002f7e374c689dcbc013188282c
roujiawen <wenroujia@gmail.com>
2018-08-14 20:43:24 -0400

    Save 'catastrophy' cases rather than raising errors; added more verbose outputs; 'sample'->'test_cases'; changed feasibility boundary from 1/3 to 1/2; save to disk each time a new sample is generated

--- gen_test_case.m
+++ gen_test_case.m
@@ -1,6 +1,6 @@
-function samples = gen_test_case(fea, n_sample)
+function test_cases = gen_test_case(fea, n_sample, filepath)
 %% PARAMS
-verbose = 0;
+verbose = 1;
 use_interval = true;
 l_out = 2;
 h_out = 4;
@@ -11,20 +11,24 @@ if nargin < 2
     n_sample = 1;
 end
 
+if nargin < 3
+    filepath = [];
+end
 %%
 % Get eta at 10^-3 around feasibility border
 if fea%feasible
-    eta = 1/3+1e-3;
+    eta = 1/2+1e-3;
 else%infeasible
-    eta = 1/3-1e-3;
+    eta = 1/2-1e-3;
 end
 
 % Get 2-outcome distribution
 out2_dist = inter_dist(eta, [], [], use_interval);
 
-%% Generating N random samples
-samples = intval(zeros(h_out^nvar, n_sample));
+%% Generating N random test_cases
+test_cases = intval(zeros(h_out^nvar, n_sample));
 for k = 1:n_sample
+    disp(k);
     certified = false;
     while ~certified%Keep generating new sample until certified
         % Blow-up to 4-outcome randomly
@@ -34,29 +38,53 @@ for k = 1:n_sample
             disp('Verifying with VSDP');
         end
         [info, best_ub, best_ub_info, best_lb, best_lb_info] =...
-            verify_spiral_fea(h_out, out4_dist, true);
+            verify_spiral_fea(h_out, out4_dist, use_interval);
         % Display output
         if verbose>0
             disp(info);
+            %disp(['  #' best_ub_info]);
+            %disp(['  #' best_lb_info]);
         end
         % Certified?
         if fea && strcmp(info, 'Certified Feasible')
             % Try again with mid values
+            if verbose>0
+                disp('Try again with mid()');
+            end
             [info, best_ub, best_ub_info, best_lb, best_lb_info] =...
                 verify_spiral_fea(h_out, mid(out4_dist), false);
             if strcmp(info, 'Certified Feasible')
+                if verbose>0
+                    disp('Certified Feasible with mid()');
+                    %disp(['  #' best_ub_info]);
+                    %disp(['  #' best_lb_info]);
+                end
                 certified = true;
             else
-                error('VSDP catastrophy');
+                disp('VSDP catastrophy');
+                dt = datestr(datetime('now'), 'mmm-dd-yyyy_HH.MM');
+                cata_path = sprintf('logs/cata_%s.mat', dt);
+                save(cata_path, 'out4_dist');
             end
         elseif ~fea && strcmp(info, 'Certified Infeasible')
             % Try again with mid values
+            if verbose>0
+                disp('Try again with mid()');
+            end
             [info, best_ub, best_ub_info, best_lb, best_lb_info] =...
                 verify_spiral_fea(h_out, mid(out4_dist), false);
             if strcmp(info, 'Certified Infeasible')
+                if verbose>0
+                    disp('Certified Infeasible with mid()');
+                    %disp(['  #' best_ub_info]);
+                    %disp(['  #' best_lb_info]);
+                end
                 certified = true;
             else
-                error('VSDP catastrophy');
+                disp('VSDP catastrophy');
+                dt = datestr(datetime('now'), 'mmm-dd-yyyy_HH.MM');
+                cata_path = sprintf('logs/cata_%s.mat', dt);
+                save(cata_path, 'out4_dist');
             end
         else %Otherwise not certified
             if verbose>0
@@ -64,8 +92,9 @@ for k = 1:n_sample
             end
         end
     end
-    
-    disp(k);
-    samples(:, k) = out4_dist;
+    test_cases(:, k) = out4_dist;
+    if ~isempty(filepath)
+        save(filepath, 'test_cases');
+    end
 end
 end
